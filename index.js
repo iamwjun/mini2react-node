add9a2da0ef702d795ccfe57a147192ede3a1b85
iamwjun <cddxwujun@hotmail.com>
2025-06-16 10:08:25 +0800

    chore: update index.js

--- index.js
+++ index.js
@@ -32,24 +32,24 @@ switch (platform) {
   case 'android':
     switch (arch) {
       case 'arm64':
-        localFileExisted = existsSync(join(__dirname, '@miniblocks/mini2react.android-arm64.node'))
+        localFileExisted = existsSync(join(__dirname, 'mini2react.android-arm64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./@miniblocks/mini2react.android-arm64.node')
+            nativeBinding = require('./mini2react.android-arm64.node')
           } else {
-            nativeBinding = require('@napi-rs/@miniblocks/mini2react-android-arm64')
+            nativeBinding = require('@miniblocks/mini2react-android-arm64')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'arm':
-        localFileExisted = existsSync(join(__dirname, '@miniblocks/mini2react.android-arm-eabi.node'))
+        localFileExisted = existsSync(join(__dirname, 'mini2react.android-arm-eabi.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./@miniblocks/mini2react.android-arm-eabi.node')
+            nativeBinding = require('./mini2react.android-arm-eabi.node')
           } else {
-            nativeBinding = require('@napi-rs/@miniblocks/mini2react-android-arm-eabi')
+            nativeBinding = require('@miniblocks/mini2react-android-arm-eabi')
           }
         } catch (e) {
           loadError = e
@@ -62,36 +62,42 @@ switch (platform) {
   case 'win32':
     switch (arch) {
       case 'x64':
-        localFileExisted = existsSync(join(__dirname, '@miniblocks/mini2react.win32-x64-msvc.node'))
+        localFileExisted = existsSync(
+          join(__dirname, 'mini2react.win32-x64-msvc.node')
+        )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./@miniblocks/mini2react.win32-x64-msvc.node')
+            nativeBinding = require('./mini2react.win32-x64-msvc.node')
           } else {
-            nativeBinding = require('@napi-rs/@miniblocks/mini2react-win32-x64-msvc')
+            nativeBinding = require('@miniblocks/mini2react-win32-x64-msvc')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'ia32':
-        localFileExisted = existsSync(join(__dirname, '@miniblocks/mini2react.win32-ia32-msvc.node'))
+        localFileExisted = existsSync(
+          join(__dirname, 'mini2react.win32-ia32-msvc.node')
+        )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./@miniblocks/mini2react.win32-ia32-msvc.node')
+            nativeBinding = require('./mini2react.win32-ia32-msvc.node')
           } else {
-            nativeBinding = require('@napi-rs/@miniblocks/mini2react-win32-ia32-msvc')
+            nativeBinding = require('@miniblocks/mini2react-win32-ia32-msvc')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'arm64':
-        localFileExisted = existsSync(join(__dirname, '@miniblocks/mini2react.win32-arm64-msvc.node'))
+        localFileExisted = existsSync(
+          join(__dirname, 'mini2react.win32-arm64-msvc.node')
+        )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./@miniblocks/mini2react.win32-arm64-msvc.node')
+            nativeBinding = require('./mini2react.win32-arm64-msvc.node')
           } else {
-            nativeBinding = require('@napi-rs/@miniblocks/mini2react-win32-arm64-msvc')
+            nativeBinding = require('@miniblocks/mini2react-win32-arm64-msvc')
           }
         } catch (e) {
           loadError = e
@@ -102,35 +108,37 @@ switch (platform) {
     }
     break
   case 'darwin':
-    localFileExisted = existsSync(join(__dirname, '@miniblocks/mini2react.darwin-universal.node'))
+    localFileExisted = existsSync(join(__dirname, 'mini2react.darwin-universal.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./@miniblocks/mini2react.darwin-universal.node')
+        nativeBinding = require('./mini2react.darwin-universal.node')
       } else {
-        nativeBinding = require('@napi-rs/@miniblocks/mini2react-darwin-universal')
+        nativeBinding = require('@miniblocks/mini2react-darwin-universal')
       }
       break
     } catch {}
     switch (arch) {
       case 'x64':
-        localFileExisted = existsSync(join(__dirname, '@miniblocks/mini2react.darwin-x64.node'))
+        localFileExisted = existsSync(join(__dirname, 'mini2react.darwin-x64.node'))
         try {
           if (localFileExisted) {
-            nativeBinding = require('./@miniblocks/mini2react.darwin-x64.node')
+            nativeBinding = require('./mini2react.darwin-x64.node')
           } else {
-            nativeBinding = require('@napi-rs/@miniblocks/mini2react-darwin-x64')
+            nativeBinding = require('@miniblocks/mini2react-darwin-x64')
           }
         } catch (e) {
           loadError = e
         }
         break
       case 'arm64':
-        localFileExisted = existsSync(join(__dirname, '@miniblocks/mini2react.darwin-arm64.node'))
+        localFileExisted = existsSync(
+          join(__dirname, 'mini2react.darwin-arm64.node')
+        )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./@miniblocks/mini2react.darwin-arm64.node')
+            nativeBinding = require('./mini2react.darwin-arm64.node')
           } else {
-            nativeBinding = require('@napi-rs/@miniblocks/mini2react-darwin-arm64')
+            nativeBinding = require('@miniblocks/mini2react-darwin-arm64')
           }
         } catch (e) {
           loadError = e
@@ -144,12 +152,12 @@ switch (platform) {
     if (arch !== 'x64') {
       throw new Error(`Unsupported architecture on FreeBSD: ${arch}`)
     }
-    localFileExisted = existsSync(join(__dirname, '@miniblocks/mini2react.freebsd-x64.node'))
+    localFileExisted = existsSync(join(__dirname, 'mini2react.freebsd-x64.node'))
     try {
       if (localFileExisted) {
-        nativeBinding = require('./@miniblocks/mini2react.freebsd-x64.node')
+        nativeBinding = require('./mini2react.freebsd-x64.node')
       } else {
-        nativeBinding = require('@napi-rs/@miniblocks/mini2react-freebsd-x64')
+        nativeBinding = require('@miniblocks/mini2react-freebsd-x64')
       }
     } catch (e) {
       loadError = e
@@ -159,23 +167,27 @@ switch (platform) {
     switch (arch) {
       case 'x64':
         if (isMusl()) {
-          localFileExisted = existsSync(join(__dirname, '@miniblocks/mini2react.linux-x64-musl.node'))
+          localFileExisted = existsSync(
+            join(__dirname, 'mini2react.linux-x64-musl.node')
+          )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./@miniblocks/mini2react.linux-x64-musl.node')
+              nativeBinding = require('./mini2react.linux-x64-musl.node')
             } else {
-              nativeBinding = require('@napi-rs/@miniblocks/mini2react-linux-x64-musl')
+              nativeBinding = require('@miniblocks/mini2react-linux-x64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
-          localFileExisted = existsSync(join(__dirname, '@miniblocks/mini2react.linux-x64-gnu.node'))
+          localFileExisted = existsSync(
+            join(__dirname, 'mini2react.linux-x64-gnu.node')
+          )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./@miniblocks/mini2react.linux-x64-gnu.node')
+              nativeBinding = require('./mini2react.linux-x64-gnu.node')
             } else {
-              nativeBinding = require('@napi-rs/@miniblocks/mini2react-linux-x64-gnu')
+              nativeBinding = require('@miniblocks/mini2react-linux-x64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -184,23 +196,27 @@ switch (platform) {
         break
       case 'arm64':
         if (isMusl()) {
-          localFileExisted = existsSync(join(__dirname, '@miniblocks/mini2react.linux-arm64-musl.node'))
+          localFileExisted = existsSync(
+            join(__dirname, 'mini2react.linux-arm64-musl.node')
+          )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./@miniblocks/mini2react.linux-arm64-musl.node')
+              nativeBinding = require('./mini2react.linux-arm64-musl.node')
             } else {
-              nativeBinding = require('@napi-rs/@miniblocks/mini2react-linux-arm64-musl')
+              nativeBinding = require('@miniblocks/mini2react-linux-arm64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
-          localFileExisted = existsSync(join(__dirname, '@miniblocks/mini2react.linux-arm64-gnu.node'))
+          localFileExisted = existsSync(
+            join(__dirname, 'mini2react.linux-arm64-gnu.node')
+          )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./@miniblocks/mini2react.linux-arm64-gnu.node')
+              nativeBinding = require('./mini2react.linux-arm64-gnu.node')
             } else {
-              nativeBinding = require('@napi-rs/@miniblocks/mini2react-linux-arm64-gnu')
+              nativeBinding = require('@miniblocks/mini2react-linux-arm64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -209,23 +225,27 @@ switch (platform) {
         break
       case 'arm':
         if (isMusl()) {
-          localFileExisted = existsSync(join(__dirname, '@miniblocks/mini2react.linux-arm-musleabihf.node'))
+          localFileExisted = existsSync(
+            join(__dirname, 'mini2react.linux-arm-musleabihf.node')
+          )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./@miniblocks/mini2react.linux-arm-musleabihf.node')
+              nativeBinding = require('./mini2react.linux-arm-musleabihf.node')
             } else {
-              nativeBinding = require('@napi-rs/@miniblocks/mini2react-linux-arm-musleabihf')
+              nativeBinding = require('@miniblocks/mini2react-linux-arm-musleabihf')
             }
           } catch (e) {
             loadError = e
           }
         } else {
-          localFileExisted = existsSync(join(__dirname, '@miniblocks/mini2react.linux-arm-gnueabihf.node'))
+          localFileExisted = existsSync(
+            join(__dirname, 'mini2react.linux-arm-gnueabihf.node')
+          )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./@miniblocks/mini2react.linux-arm-gnueabihf.node')
+              nativeBinding = require('./mini2react.linux-arm-gnueabihf.node')
             } else {
-              nativeBinding = require('@napi-rs/@miniblocks/mini2react-linux-arm-gnueabihf')
+              nativeBinding = require('@miniblocks/mini2react-linux-arm-gnueabihf')
             }
           } catch (e) {
             loadError = e
@@ -234,23 +254,27 @@ switch (platform) {
         break
       case 'riscv64':
         if (isMusl()) {
-          localFileExisted = existsSync(join(__dirname, '@miniblocks/mini2react.linux-riscv64-musl.node'))
+          localFileExisted = existsSync(
+            join(__dirname, 'mini2react.linux-riscv64-musl.node')
+          )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./@miniblocks/mini2react.linux-riscv64-musl.node')
+              nativeBinding = require('./mini2react.linux-riscv64-musl.node')
             } else {
-              nativeBinding = require('@napi-rs/@miniblocks/mini2react-linux-riscv64-musl')
+              nativeBinding = require('@miniblocks/mini2react-linux-riscv64-musl')
             }
           } catch (e) {
             loadError = e
           }
         } else {
-          localFileExisted = existsSync(join(__dirname, '@miniblocks/mini2react.linux-riscv64-gnu.node'))
+          localFileExisted = existsSync(
+            join(__dirname, 'mini2react.linux-riscv64-gnu.node')
+          )
           try {
             if (localFileExisted) {
-              nativeBinding = require('./@miniblocks/mini2react.linux-riscv64-gnu.node')
+              nativeBinding = require('./mini2react.linux-riscv64-gnu.node')
             } else {
-              nativeBinding = require('@napi-rs/@miniblocks/mini2react-linux-riscv64-gnu')
+              nativeBinding = require('@miniblocks/mini2react-linux-riscv64-gnu')
             }
           } catch (e) {
             loadError = e
@@ -258,12 +282,14 @@ switch (platform) {
         }
         break
       case 's390x':
-        localFileExisted = existsSync(join(__dirname, '@miniblocks/mini2react.linux-s390x-gnu.node'))
+        localFileExisted = existsSync(
+          join(__dirname, 'mini2react.linux-s390x-gnu.node')
+        )
         try {
           if (localFileExisted) {
-            nativeBinding = require('./@miniblocks/mini2react.linux-s390x-gnu.node')
+            nativeBinding = require('./mini2react.linux-s390x-gnu.node')
           } else {
-            nativeBinding = require('@napi-rs/@miniblocks/mini2react-linux-s390x-gnu')
+            nativeBinding = require('@miniblocks/mini2react-linux-s390x-gnu')
           }
         } catch (e) {
           loadError = e
